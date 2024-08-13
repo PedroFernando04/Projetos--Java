27a75431a0ebdb41b846680e2a1cc4eac834a442
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-08-13 15:57:49 -0300

    Add files via upload

---
+++ 2-periodo/Lista01/Q3.java
@@ -0,0 +1,20 @@
+//Faça um programa que verifique se um letra digitada é uma vogal ou uma consoante;
+
+import java.util.Scanner;
+
+public class Q3 {
+    public static void main(String[] args) {
+        Scanner scanf = new Scanner(System.in);
+
+        System.out.println("Digite uma letra");
+        char letra = scanf.next().toLowerCase().charAt(0);
+
+        if(letra == 'a'|| letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u')
+            System.out.println("É uma vogal!");
+        else
+            System.out.println("É uma consoante!");
+
+        
+
+    }
+}
