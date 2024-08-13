c1687a28186836111613c05cc8bb869de0932247
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-08-13 13:29:51 -0300

    Add files via upload

---
+++ 2-periodo/Q1.java
@@ -0,0 +1,23 @@
+/* 
+ Faça um Programa que peça um número inteiro e determine se ele é par ou ímpar. 
+ Dica: utilize o operador módulo (resto da divisão[%])
+ */
+
+import java.util.Scanner;
+
+public class Q1 {
+    public static void main(String[] args){
+        Scanner scanf = new Scanner(System.in);
+    
+        int numero;
+        System.out.println("Informe um número inteiro: ");
+        numero = scanf.nextInt();
+    
+        if(numero % 2 == 0)
+            System.out.println("É par!");
+        else
+            System.out.println("É ímpar!");
+    }
+   
+
+}
