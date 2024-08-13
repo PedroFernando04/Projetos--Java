fde7f036727aa6a1a3273ee7b477c5250088b0cd
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-08-13 17:02:44 -0300

    Add files via upload

---
+++ 2-periodo/Lista01/Q4.java
@@ -0,0 +1,34 @@
+/*
+    Faça um programa para a leitura de duas notas parciais de um aluno. O programa deve calcular 
+a média alcançada por um aluno e apresentar:
+        a. A mensagem "Aprovado", se a média alcançada for maior ou igual a sete
+        b. A mensagem "Reprovado", se a média for menor do que sete
+        c. A mensagem "Aprovado com Distinção", se a média for igual a dez
+ */
+
+ import java.util.Scanner;
+
+public class Q4 {
+    public static void main(String[] args){
+        Scanner scanf = new Scanner(System.in);
+
+        float nota = 0f;
+
+        for (int i = 0; i < 2; i++) {
+            System.out.println("Informe sua " + (i + 1) + "° nota: ");
+            nota += scanf.nextFloat();
+        }
+        
+        float media = nota/2;
+
+        if(media == 10)
+            System.out.println("Aprovado com Distinção");
+        else if(media >= 7)
+            System.out.println("Aprovado");
+        else
+            System.out.println("Reprovado");
+
+
+
+    }
+}
