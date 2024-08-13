c073513f2e53dafd8e05451707261859fb5c809c
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-08-13 13:29:22 -0300

    Add files via upload

---
+++ 2-periodo/Exercicios01/Ex2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        Scanner scanf = new Scanner(System.in);
+        float valor, desconto = 0.85f;
+
+        System.out.println("Informe o valor do seu produto: ");
+        valor = scanf.nextFloat();
+
+        System.out.println("Valor total: " + (valor*desconto));
+        System.out.println("(Valor do produto: " + valor + " - Valor do desconto: " + (valor - valor*desconto) + ")");
+    }  
+}
