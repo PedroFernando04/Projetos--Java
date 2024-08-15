c0160ce54a0c498595c72f3f68574b3ca0286fb4
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-08-15 09:10:56 -0300

    Create ExD1.java

---
+++ 2-periodo/Lista02/Exercicios(Desafios)/ExD1.java
@@ -0,0 +1,23 @@
+/*
+ A série de Fibonacci é formada pela sequência 0,1,1,2,3,5,8,13,21,34,55,...
+ Faça um programa que gere a série até que o valor seja maior que 500.
+ */
+
+public class ExD1 {
+    public static void main(String[] args) {
+        int i = 0, auxiliar = 1, fibo;
+
+        while(true){
+            if(i > 500){
+                System.out.println(i);
+                break;
+            }
+            fibo = i + auxiliar;
+
+            System.out.println(i);
+ 
+            i = auxiliar;
+            auxiliar = fibo;
+        }
+    }
+}
