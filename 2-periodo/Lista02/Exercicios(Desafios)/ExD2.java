faef6a1a4ae96deb696208063503108a5689bb6b
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-08-15 09:12:00 -0300

    Add files via upload

---
+++ 2-periodo/Lista02/Exercicios(Desafios)/ExD2.java
@@ -0,0 +1,17 @@
+/*
+Faça um programa que, dado um conjunto de N números, determine o menor valor,
+o maior valor e a soma dos valores
+*/
+
+import java.util.Scanner;
+
+public class ExD2 {
+    public static void main(String[] args){
+        Scanner scanf = new Scanner(System.in);
+
+        System.out.println("Infome a quantidade de números que serão inseridos: ");
+        float N = scanf.nextFloat();
+    
+    }
+    
+}
