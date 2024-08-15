25578456d7ad8c0b228b27665d0c372940bba1f2
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-08-15 09:11:32 -0300

    Add files via upload

---
+++ 2-periodo/Lista02/Exercicios(Básico)/ExB2.java
@@ -0,0 +1,17 @@
+/*Faça um programa, utilizando while, que mostre na tela de 0 até N, em que N é o limite 
+inserido pelo usuário*/
+import java.util.Scanner;
+
+public class ExB2 {
+    public static void main(String[] args){
+        Scanner scanf = new Scanner(System.in);
+        
+        System.out.println("Informe um número inteiro: ");
+        int N = scanf.nextInt();
+
+        System.out.println("\n");
+
+        for (int i = 0; i <= N; i++) 
+            System.out.println(i);
+    }
+}
---
+++ 2-periodo/Lista02/Exercicios(Básico)/ExB3.java
@@ -0,0 +1,35 @@
+/*
+ Faça um programa, utilizando while, que permita o usuário fazer contas de adição enquanto quiser
+ --Deseja realizar mais uma soma? [S ou N]--
+ */
+import java.util.Scanner;
+
+public class ExB3 {
+    public static void main(String[] args) {
+        Scanner scanf = new Scanner(System.in);
+        
+        System.out.println("Operação - Adição");
+
+        while(true){
+            System.out.println("\nInforme o primeiro número: ");
+            float n1 = scanf.nextFloat();
+
+            System.out.println("\nInforme o segundo número:");
+            float n2 = scanf.nextFloat();
+
+            float soma = n1 + n2;
+            System.out.println("\n"+n1 + " + " + n2 + " = " + soma+'\n');
+
+            System.out.println("Deseja realizar mais uma soma? [S ou N]");
+            char loop = scanf.next().toLowerCase().charAt(0);
+
+            if(loop == 'n')
+                break;
+            else if(loop != 'n' && loop != 's'){
+                System.out.println("\nValor inválido!");
+                System.out.println("Deseja realizar mais uma soma? [S ou N]");
+                loop = scanf.next().toLowerCase().charAt(0);
+            }
+        }
+    }
+}
