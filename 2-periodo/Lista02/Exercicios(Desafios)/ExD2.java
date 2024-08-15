16b49390572ecbc15176a9e84065070dbc267eef
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-08-15 12:48:27 -0300

    Update ExD2.java

--- 2-periodo/Lista02/Exercicios(Desafios)/ExD2.java
+++ 2-periodo/Lista02/Exercicios(Desafios)/ExD2.java
@@ -9,9 +9,25 @@ public class ExD2 {
     public static void main(String[] args){
         Scanner scanf = new Scanner(System.in);
 
-        System.out.println("Infome a quantidade de números que serão inseridos: ");
-        float N = scanf.nextFloat();
-    
+        System.out.println("\nInfome a quantidade de números que serão inseridos: ");
+        int N = scanf.nextInt();
+
+        System.out.println("Digite o 1° valor: ");
+        float numeros = scanf.nextFloat();
+        float maior = numeros, menor = numeros, total = numeros;
+
+        for (int i = 2; i <= N; i++) {
+            System.out.println("Digite seu "+ i + "° valor: ");
+            numeros = scanf.nextFloat();
+            total += numeros;
+            if (maior < numeros)
+                maior = numeros;
+            if(menor > numeros)
+                menor = numeros;
+        }
+        System.out.println("\nMenor valor: " + menor);
+        System.out.println("Maior valor: " + maior);
+        System.out.println("Soma dos valores: " + total);
     }
     
 }
