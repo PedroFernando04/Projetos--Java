245dd1eb1c5170a819293bc86e0b01c5a955134a
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-09-02 11:30:10 -0300

    Create Rev01_1.java

---
+++ 2-periodo/RevisãoAV01/Rev01_1.java
@@ -0,0 +1,34 @@
+/*
+ Crie um programa que leia um valor inteiro e exiba todos os números ímpares no intervalo de 1 a esse valor, separando-os.
+
+ valor = 10
+ saída:
+ Ímpares até 10: 1, 3, 5, 7, 9
+ Pares até 10: 2, 4, 6, 8, 10
+ */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Rev01_1{
+    public static void main(String[] args) {
+        Scanner scanf = new Scanner(System.in);
+
+        System.out.println("Informe um valor inteiro: ");
+        int numero = scanf.nextInt();
+
+        ArrayList<Integer> impares = new ArrayList<>();
+        ArrayList<Integer> pares = new ArrayList<>();
+
+        for(int i = 1; i <= numero; i++){
+            if (i % 2 == 0)
+                pares.add(i);
+            
+            else
+                impares.add(i);
+        }
+
+        System.out.println("Número pares até " + numero + ": " + pares);
+        System.out.println("Número ímpares até " + numero + ": " + impares);
+    }
+}
