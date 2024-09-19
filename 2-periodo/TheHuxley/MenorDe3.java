2137042b0a03a4c3ae33baf366f88275740bb24c
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-09-19 12:16:33 -0300

    Create MenorDe3.java

---
+++ 2-periodo/TheHuxley/MenorDe3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class MenorDe3 {
+    public static void main(String[] args) {
+        Scanner scanf = new Scanner(System.in);
+        int[] lista = {1, 2, 3};
+
+
+        for(int i = 0; i < 3; i++){
+            lista[i] = scanf.nextInt();
+            //"informe o " + (i + 1) + " número: "
+        }
+
+        if(lista[0] < lista[1] && lista[0] < lista[2])
+            System.out.println(lista[0]);
+        else if(lista[1] < lista[2] && lista[1] < lista[2])
+            System.out.println(lista[1]);
+        else
+            System.out.println(lista[2]);
+    }
+}
