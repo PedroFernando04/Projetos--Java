ef64e16a01ff14dca5c335549fe0310f6a8f7102
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-09-19 17:02:28 -0300

    Update and rename MenorDe3.java to menorDe3.java

--- 2-periodo/TheHuxley/menorDe3.java
+++ 2-periodo/TheHuxley/menorDe3.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 public class MenorDe3 {
     public static void main(String[] args) {
         Scanner scanf = new Scanner(System.in);
-        int[] lista = {1, 2, 3};
+        int[] lista = new int[3];
 
 
         for(int i = 0; i < 3; i++){
             lista[i] = scanf.nextInt();
             //"informe o " + (i + 1) + " número: "
+
+            //if(lista[i] < lista[i + 1])
         }
 
         if(lista[0] < lista[1] && lista[0] < lista[2])
