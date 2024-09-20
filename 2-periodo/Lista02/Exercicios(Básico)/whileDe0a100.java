daf0e0a012286df258004757ca16bd9043cdd328
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-09-20 13:02:55 -0300

    Update and rename ExB1.java to whileDe0a100.java

--- 2-periodo/Lista02/Exercicios(Básico)/whileDe0a100.java
+++ 2-periodo/Lista02/Exercicios(Básico)/whileDe0a100.java
@@ -1,6 +1,6 @@
 //Faça um programa, utilizando while, que mostre na tela os números de 0 a 100
 
-public class ExB1 {
+public class whileDe0a100 {
     public static void main(String[] args){
         int i = 0;
         while(i <= 100){
