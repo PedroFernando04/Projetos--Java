95adf1b21fb19a7689ca8381de2140c0e8a32040
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-09-20 13:05:50 -0300

    Update and rename ExD4.java to validacaoFormulario.java

--- 2-periodo/Lista02/Exercicios(Desafios)/validacaoFormulario.java
+++ 2-periodo/Lista02/Exercicios(Desafios)/validacaoFormulario.java
@@ -10,7 +10,7 @@
 
  import java.util.Scanner;
 
-public class ExD4 {
+public class validacaoFormulario {
    public static void main(String[] args) {
       Scanner scanf = new Scanner(System.in);
       String nome;
