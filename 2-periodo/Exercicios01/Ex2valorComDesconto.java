f0c292788ffd47c0f79503df09769982f1cbd455
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-09-20 12:01:04 -0300

    Update and rename Ex2ValorComDesconto.java to Ex2valorComDesconto.java

--- 2-periodo/Exercicios01/Ex2valorComDesconto.java
+++ 2-periodo/Exercicios01/Ex2valorComDesconto.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Ex2ValorComDesconto {
+public class Ex2valorComDesconto {
     public static void main(String[] args) {
         Scanner scanf = new Scanner(System.in);
         float valor, desconto = 0.85f;
