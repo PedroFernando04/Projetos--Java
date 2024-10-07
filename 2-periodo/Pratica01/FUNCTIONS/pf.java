5ee84feae92fb4d150528b37c3f2ff8ffba07ab2
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-10-07 14:07:07 -0300

    Update pf.java

--- 2-periodo/Pratica01/FUNCTIONS/pf.java
+++ 2-periodo/Pratica01/FUNCTIONS/pf.java
@@ -1,18 +1,21 @@
+    /* 
+
 //Q1
+public static void positivoOuNegativo(double numero){
+    if(numero > 0)
+        System.out.println('P');
+    else   
+        System.out.println('N');
+}
 
-  public static void positivoOuNegativo(double numero){
-        if(numero > 0)
-            System.out.println('P');
-        else   
-            System.out.println('N');
 //Q2
-
 public static int quantidadeDeDigitos(int numero, int contador){
-        numero /= 10;
-        contador++;
+    numero /= 10;
+    contador++;
 
-        if(numero < 1)
-            return contador;
-        else
-            return quantidadeDeDigitos(numero, contador);
-//Q3
+    if(numero < 1)
+        return contador;
+    else
+        return quantidadeDeDigitos(numero, contador);
+}
+    */
