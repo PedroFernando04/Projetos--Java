26aad32d77f8bb687a0c484424a88da827ad7f2c
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2024-10-07 16:05:56 -0300

    Update pf.java

--- 2-periodo/Pratica01/FUNCTIONS/pf.java
+++ 2-periodo/Pratica01/FUNCTIONS/pf.java
@@ -1,6 +1,7 @@
     /* 
 
 //Q1
+
 public static void positivoOuNegativo(double numero){
     if(numero > 0)
         System.out.println('P');
@@ -9,6 +10,7 @@ public static void positivoOuNegativo(double numero){
 }
 
 //Q2
+
 public static int quantidadeDeDigitos(int numero, int contador){
     numero /= 10;
     contador++;
@@ -18,4 +20,34 @@ public static int quantidadeDeDigitos(int numero, int contador){
     else
         return quantidadeDeDigitos(numero, contador);
 }
+
+//Q3
+
+public static double somar(double n1, double n2){
+        return n1 + n2;
+    }
+
+    public static double subtrair(double n1, double n2){
+        return n1 - n2;
+    }
+
+    public static double multiplicar(double n1, double n2){
+        return n1 * n2;
+    }
+
+    public static double dividir(double n1, double n2){
+        Scanner scanf = new Scanner(System.in);
+
+        if(n2 != 0){
+            scanf.close();
+           return n1 / n2;
+        }
+        else{
+            System.out.println("\nERRO: Divisão por 0\n");
+            System.out.print("Informe o 2° número novamente: ");
+            n2 = scanf.nextDouble();
+            return dividir(n1, n2);   
+        }
+    }
+    
     */
