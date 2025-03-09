bd67b7a4a339351c6e71540492419cad5e2f590e
Pedro Fernando <106192736+PedroFernando04@users.noreply.github.com>
2025-03-09 16:03:33 -0300

    Create Q3.java

---
+++ 3-periodo/EstruturaDeDados/fila&pilha/Q3.java
@@ -0,0 +1,108 @@
+class No
+{
+    String valor;
+    No proximo;
+
+    No(String valorAtual)
+    {
+        valor = valorAtual;
+        proximo = null;
+    }
+}
+
+class pilhaEncadeada
+{
+    No inicio;
+    No fim;
+    int tamanho;
+
+    pilhaEncadeada()
+    {
+        inicio = fim = null;
+        tamanho = 0;
+    }
+
+    void push(String novoValor)
+    {
+        No novoNo = new No(novoValor);
+
+        if(inicio == null)
+        inicio = fim = novoNo;
+
+        else
+        {
+            novoNo.proximo = inicio;
+            inicio = novoNo;
+        }
+
+        tamanho++;
+    }
+
+    void pop()
+    {
+        boolean vazio = isEmpty();
+
+        if(vazio == false)
+        {
+            System.out.println("item removido: " + inicio.valor);
+            inicio = inicio.proximo;
+            tamanho--;
+        }
+    }
+
+    void peek()
+    {
+        System.out.println("Topo atual: " + inicio.valor);
+    }
+
+    boolean isEmpty()
+    {
+        if(inicio == null)
+        {
+            System.out.println("Pilha vazia!");
+            return true;
+        }
+        
+        else
+        {
+            System.out.println("Pilha com " + tamanho + " elemento(s)");
+            return false;
+        }
+        
+    }
+
+    void exibir()
+    {
+        No atual = inicio;
+
+        while(atual != null)
+        {
+            System.out.println(atual.valor);
+            atual = atual.proximo;
+        }
+    }
+}
+
+public class Q3
+{
+    public static void main(String[] args)
+    {
+        pilhaEncadeada pilha = new pilhaEncadeada();
+
+        pilha.isEmpty();
+
+        pilha.push("FalleN");
+        pilha.push("Fer");
+        pilha.push("Coldzera");
+        pilha.push("Taco");
+        pilha.push("FNX");
+
+        pilha.exibir();
+        pilha.isEmpty();
+        pilha.peek();
+        pilha.pop();
+
+        pilha.push("Felps");
+        pilha.exibir();
+    }
+}
